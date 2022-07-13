130a8e641f92bcafacef0544e8e75836e8bd3f0a
Laurel <41247144+laurelxiang@users.noreply.github.com>
2022-07-13 14:01:28 -0400

    added deployment using server to redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
-#automatic deployment script
+#automatic deployment using server
 cd ~
-tmux kill-session
 cd ~/portfolio-website
 git fetch && git reset origin/main --hard
 python -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
 pip3 install -U pip #for bug fix when downloading cryptography library
 pip install -r requirements.txt
-tmux new -d -s flask-host-session
-tmux send-keys -t flask-host-session "source python3-virtualenv/bin/activate" ENTER
-tmux send-keys -t flask-host-session "cd ~/portfolio-website" ENTER
-tmux send-keys -t flask-host-session "flask run --host=0.0.0.0" ENTER
+sytemctl daemon-reload
+systemctl restart myportfolio
+
+# #automatic deployment script using tmux
+# cd ~
+# tmux kill-session
+# cd ~/portfolio-website
+# git fetch && git reset origin/main --hard
+# python -m venv python3-virtualenv
+# source python3-virtualenv/bin/activate
+# pip3 install -U pip #for bug fix when downloading cryptography library
+# pip install -r requirements.txt
+# tmux new -d -s flask-host-session
+# tmux send-keys -t flask-host-session "source python3-virtualenv/bin/activate" ENTER
+# tmux send-keys -t flask-host-session "cd ~/portfolio-website" ENTER
+# tmux send-keys -t flask-host-session "flask run --host=0.0.0.0" ENTER

de039399d5df69fbc7e899c104d34a0568e0c65e
Laurel <41247144+laurelxiang@users.noreply.github.com>
2022-07-12 21:15:56 -0400

    timeline endpoints using curl created

--- local-deploy.sh
+++ local-deploy.sh
@@ -3,4 +3,17 @@
 python -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
-flask run
+
+if [ $# -eq 0 ]
+then
+    flask run
+elif [ $1 == 0 ]
+then
+    flask run
+elif [ $1 == 1 ]
+then
+    tmux new -d -s flask-host-session
+    tmux send-keys -t flask-host-session "source python3-virtualenv/bin/activate" ENTER
+    tmux send-keys -t flask-host-session "cd ~/workspace/mlh/portfolio-website" ENTER
+    tmux send-keys -t flask-host-session "flask run" ENTER
+fi

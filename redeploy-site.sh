98d655e096a87afa5d823506fbf61d8bae4597cd
Laurel <41247144+laurelxiang@users.noreply.github.com>
2022-08-05 00:24:57 -0400

    fixed typo in deployment script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #automatic deployment using docker container
-cd portfolio-website
+cd ~/portfolio-website
 git fetch && git reset origin/main --hard
 docker compose -f docker-compose.prod.yml down
 docker compose -f docker-compose.prod.yml up -d --build

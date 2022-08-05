24d132c5f9e3c4069d9b33aa4a0a45db5c4ec955
Laurel <41247144+laurelxiang@users.noreply.github.com>
2022-08-05 00:22:21 -0400

    added deployment using container script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-#automatic deployment using server
-cd ~
-cd ~/portfolio-website
+#automatic deployment using docker container
+cd portfolio-website
 git fetch && git reset origin/main --hard
-python -m venv python3-virtualenv
-source python3-virtualenv/bin/activate
-pip3 install -U pip #for bug fix when downloading cryptography library
-pip install -r requirements.txt
-systemctl daemon-reload
-systemctl restart myportfolio
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
+
+
+# #automatic deployment using systemctl server
+# cd ~/portfolio-website
+# git fetch && git reset origin/main --hard
+# python -m venv python3-virtualenv
+# source python3-virtualenv/bin/activate
+# pip3 install -U pip #for bug fix when downloading cryptography library
+# pip install -r requirements.txt
+# systemctl daemon-reload
+# systemctl restart myportfolio
 
 # #automatic deployment script using tmux
-# cd ~
 # tmux kill-session
 # cd ~/portfolio-website
 # git fetch && git reset origin/main --hard

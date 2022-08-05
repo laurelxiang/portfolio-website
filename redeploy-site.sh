b6a164282b09a46a467bd9ba98572eadf86fc322
Laurel <41247144+laurelxiang@users.noreply.github.com>
2022-08-05 00:25:52 -0400

    cleaned deployment script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -6,7 +6,6 @@ git fetch && git reset origin/main --hard
 docker compose -f docker-compose.prod.yml down
 docker compose -f docker-compose.prod.yml up -d --build
 
-
 # #automatic deployment using systemctl server
 # cd ~/portfolio-website
 # git fetch && git reset origin/main --hard

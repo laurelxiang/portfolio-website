338c522879290b4138b27be5522c99c817b38782
Laurel <41247144+laurelxiang@users.noreply.github.com>
2022-08-05 03:03:15 -0400

    timeline display

--- app/timeline.js
+++ app/timeline.js
@@ -6,10 +6,28 @@ form.addEventListener('submit', function (e) {
     console.log([...payload]);
 
     // post the payload using fetch
-    fetch('http://localhost:5000/api/timeline_post', {
+    fetch('/api/timeline_post', {
         method: 'POST',
         body: payload,
     })
         .then(res => res.json())
         .then(data => console.log(data))
 })
+
+
+fetch('/api/timeline_post').then(response => response.json())
+    .then(data =>
+        appendData(data.timeline_posts)
+    ).catch(err => {
+        console.log(err);
+    });
+
+appendData = data => {
+    const container = document.querySelector("#post-data");
+
+    data.forEach(timeline_post => {
+        const post = document.createElement('p');
+        post.innerHTML = `Name: ${timeline_post.name} <br> Email: ${timeline_post.email} <br> Content: ${timeline_post.content} <br> Created at: ${timeline_post.created_at}`;
+        container.append(post);
+    })
+}

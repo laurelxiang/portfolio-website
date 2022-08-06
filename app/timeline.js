98abea888943bb1d76f7cfd09141f75b39e58e7b
Laurel <41247144+laurelxiang@users.noreply.github.com>
2022-08-06 19:40:09 -0400

    Update timeline.js

--- app/timeline.js
+++ app/timeline.js
@@ -14,20 +14,29 @@ form.addEventListener('submit', function (e) {
         .then(data => console.log(data))
 })
 
+const posts = document.querySelector('#post-data');
 
-fetch('/api/timeline_post').then(response => response.json())
-    .then(data =>
-        appendData(data.timeline_posts)
-    ).catch(err => {
-        console.log(err);
-    });
-
-appendData = data => {
-    const container = document.querySelector("#post-data");
-
-    data.forEach(timeline_post => {
-        const post = document.createElement('p');
-        post.innerHTML = `Name: ${timeline_post.name} <br> Email: ${timeline_post.email} <br> Content: ${timeline_post.content} <br> Created at: ${timeline_post.created_at}`;
-        container.append(post);
-    })
+// fetch timeline post data from api
+const getPosts = () => {
+    fetch('/api/timeline_post')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            const posts_data = data['timeline_posts']
+            const html = posts_data.map(post2html).join('\n');
+            posts.innerHTML = html;
+        }
+        );
+    
 }
+
+// prints post
+const post2html = (post) =>{
+    return `
+        <li>Name: ${post['name']},
+            Email: ${post['email']}, 
+            Content: ${post['content']}</il>
+        `
+}
+
+getPosts();

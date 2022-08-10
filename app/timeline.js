c1dfa7a02c23a8c887df757cc66c8c92d5771209
Laurel <41247144+laurelxiang@users.noreply.github.com>
2022-08-10 00:13:30 -0400

    js move

--- app/timeline.js
+++ app/timeline.js
@@ -1,73 +1,55 @@
-//retrieve data
-fetch('/api/timeline_post').then(response => response.json())
-        .then(data =>
-            appendData(data.timeline_posts)
-        ).catch(err => {
-            console.log(err);
-        });
+//fetching data
+fetch('/api/timeline_post')
+.then(
+    response => response.json())
+.then(
+    data => appendData(data.timeline_posts)
+).catch(
+    err => {console.log(err);
+});
 
-    appendData = data => {
-        const container = document.querySelector("#post-data");
+appendData = data => {
+const container = document.querySelector("#post-data");
 
-        data.forEach(timeline_post => {
-            const post = document.createElement('p');
-            post.innerHTML = `Name: ${timeline_post.name} <br> Email: ${timeline_post.email} <br> Content: ${timeline_post.content} <br> Created at: ${timeline_post.created_at}`;
-            container.append(post);
-        })
-    }
+data.slice().reverse().forEach(timeline_post => {
+    const post = document.createElement('p');
+    post.innerHTML = `Name: ${timeline_post.name} Email: ${timeline_post.email}  Created at: ${timeline_post.created_at} <br> Content: ${timeline_post.content}`;
+    container.append(post);
+})
+}
 
-
-// fetch('/api/timeline_post')
-//     .then(function (response) {
-//         return response.json();
-//     })
-//     .then(function (data) {
-//         appendData(data);
-//     })
-//     .catch(function (err) {
-//         console.log('error: ' + err);
-//     });
-    // .then(response => 
-    //     response.json())
-    // .then(data =>
-    //     appendData(data.timeline_posts)
-    // ).catch(err => {
-    //     console.log(err);
-    // });
-
-// appendData = data => {
-//     const container = document.querySelector("#post-data");
-
-//     data.forEach(timeline_post => {
-//         const post = document.createElement('p');
-//         post.innerHTML = `Name: ${timeline_post.name} <br> Email: ${timeline_post.email} <br> Content: ${timeline_post.content} <br> Created at: ${timeline_post.created_at}`;
-//         container.append(post);
-//     })
-// }
-
-// function appendData(data) {
-//     var mainContainer = document.getElementById("post-data");
-//     data = data.timeline_posts
-//     for (var i = 0; i < data.length; i++) {
-//       var div = document.createElement("div");
-//       div.innerHTML = 'Name: ' + data[i].name + ' Email:' + data[i].email + ' Content:' + data[i].content + ' Created at:' + data[i].created_at;
-//       mainContainer.appendChild(div);
-//     }
-//   }
-
-//post data
+//posting data
 const form = document.getElementById('form');
 form.addEventListener('submit', function (e) {
-    e.preventDefault();
+e.preventDefault();
 
-    const payload = new FormData(form);
-    console.log([...payload]);
+const payload = new FormData(form);
+console.log([...payload]);
 
-    // post the payload using fetch
-    fetch('/api/timeline_post', {
-        method: 'POST',
-        body: payload,
-    })
-        .then(res => res.json())
-        .then(data => console.log(data))
+// post the payload using fetch
+fetch('/api/timeline_post', {
+    method: 'POST',
+    body: payload,
+})
+    .then(
+        res => res.json()
+    )
+    .then(
+        data => {
+            console.log(data);
+            fetch('/api/timeline_post')
+                .then(
+                    response => response.json())
+                .then(
+                    data => {
+                        const container = document.querySelector("#post-data");
+                        const post = document.createElement('p');
+                        post.innerHTML = `Name: ${data.timeline_posts[0].name} Email: ${data.timeline_posts[0].email}  Created at: ${data.timeline_posts[0].created_at} <br> Content: ${data.timeline_posts[0].content}`;
+                        container.append(post);
+                    }
+                ).catch(
+                    err => {console.log(err);
+                });
+        }
+    )
 })

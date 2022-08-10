342f3ca14c9c651c735d913e4e2e578f601a8b7c
Laurel <41247144+laurelxiang@users.noreply.github.com>
2022-08-09 21:04:07 -0400

    timeline updates

--- app/timeline.js
+++ app/timeline.js
@@ -1,42 +1,33 @@
 const form = document.getElementById('form');
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
 
-    const payload = new FormData(form);
-    console.log([...payload]);
+        const payload = new FormData(form);
+        console.log([...payload]);
 
-    // post the payload using fetch
-    fetch('/api/timeline_post', {
-        method: 'POST',
-        body: payload,
+        // post the payload using fetch
+        fetch('/api/timeline_post', {
+            method: 'POST',
+            body: payload,
+        })
+            .then(res => res.json())
+            .then(data => console.log(data))
     })
-        .then(res => res.json())
-        .then(data => console.log(data))
-})
 
-const posts = document.querySelector('#post-data');
 
-// fetch timeline post data from api
-const getPosts = () => {
-    fetch('/api/timeline_post')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            const posts_data = data['timeline_posts']
-            const html = posts_data.map(post2html).join('\n');
-            posts.innerHTML = html;
-        }
-        );
-    
-}
+    fetch('/api/timeline_post').then(response => response.json())
+        .then(data =>
+            appendData(data.timeline_posts)
+        ).catch(err => {
+            console.log(err);
+        });
 
-// prints post
-const post2html = (post) =>{
-    return `
-        <li>Name: ${post['name']},
-            Email: ${post['email']}, 
-            Content: ${post['content']}</il>
-        `
-}
+    appendData = data => {
+        const container = document.querySelector("#post-data");
 
-getPosts();
+        data.forEach(timeline_post => {
+            const post = document.createElement('p');
+            post.innerHTML = `Name: ${timeline_post.name} <br> Email: ${timeline_post.email} <br> Content: ${timeline_post.content} <br> Created at: ${timeline_post.created_at}`;
+            container.append(post);
+        })
+    }

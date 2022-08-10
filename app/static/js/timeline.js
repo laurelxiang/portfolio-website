0950ee2515b56b28e60ebd7d057978150c80eed3
Laurel <41247144+laurelxiang@users.noreply.github.com>
2022-08-10 00:34:42 -0400

    code cleanup

--- app/static/js/timeline.js
+++ app/static/js/timeline.js
@@ -1,55 +1,55 @@
-//fetching data
-fetch('/api/timeline_post')
-.then(
-    response => response.json())
-.then(
-    data => appendData(data.timeline_posts)
-).catch(
-    err => {console.log(err);
-});
+// //fetching data
+// fetch('/api/timeline_post')
+// .then(
+//     response => response.json())
+// .then(
+//     data => appendData(data.timeline_posts)
+// ).catch(
+//     err => {console.log(err);
+// });
 
-appendData = data => {
-const container = document.querySelector("#publish");
+// appendData = data => {
+// const container = document.querySelector("#publish");
 
-data.slice().reverse().forEach(tpost => {
-    const post = document.createElement('p');
-    post.innerHTML = `Name: ${tpost.name} Email: ${tpost.email}  Created at: ${tpost.created_at} <br> Content: ${tpost.content}`;
-    container.append(post);
-})
-}
+// data.slice().reverse().forEach(tpost => {
+//     const post = document.createElement('p');
+//     post.innerHTML = `Name: ${tpost.name} Email: ${tpost.email}  Created at: ${tpost.created_at} <br> Content: ${tpost.content}`;
+//     container.append(post);
+// })
+// }
 
-//posting data
-const form = document.getElementById('form');
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
+// //posting data
+// const form = document.getElementById('form');
+// form.addEventListener('submit', function (e) {
+//     e.preventDefault();
 
-    const payload = new FormData(form);
-    console.log([...payload]);
+//     const payload = new FormData(form);
+//     console.log([...payload]);
 
-    fetch('/api/timeline_post', {
-        method: 'POST',
-        body: payload,
-    })
-        .then(
-            res => res.json()
-        )
-        .then(
-            data => {
-                console.log(data);
-                fetch('/api/timeline_post')
-                    .then(
-                        response => response.json())
-                    .then(
-                        data => {
-                            const container = document.querySelector("#publish");
-                            const post = document.createElement('p');
-                            post.innerHTML = `Name: ${data.timeline_posts[0].name} Email: ${data.timeline_posts[0].email}  Created at: ${data.timeline_posts[0].created_at} <br> Content: ${data.timeline_posts[0].content}`;
-                            container.append(post);
-                        }
-                    ).catch(
-                        err => {console.log(err);
-                    });
-            }
-        )
-    }
-)
+//     fetch('/api/timeline_post', {
+//         method: 'POST',
+//         body: payload,
+//     })
+//         .then(
+//             res => res.json()
+//         )
+//         .then(
+//             data => {
+//                 console.log(data);
+//                 fetch('/api/timeline_post')
+//                     .then(
+//                         response => response.json())
+//                     .then(
+//                         data => {
+//                             const container = document.querySelector("#publish");
+//                             const post = document.createElement('p');
+//                             post.innerHTML = `Name: ${data.timeline_posts[0].name} Email: ${data.timeline_posts[0].email}  Created at: ${data.timeline_posts[0].created_at} <br> Content: ${data.timeline_posts[0].content}`;
+//                             container.append(post);
+//                         }
+//                     ).catch(
+//                         err => {console.log(err);
+//                     });
+//             }
+//         )
+//     }
+// )
